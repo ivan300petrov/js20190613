b3ffad75159acc5feb1148fea8f41e46c2f9da51
ivan300petrov <38078954+ivan300petrov@users.noreply.github.com>
2019-08-07 17:34:51 +0700

    Update Component.js

--- scripts/components/Component/Component.js
+++ scripts/components/Component/Component.js
@@ -4,9 +4,10 @@ export class Component {
   }
   onHeadClick(e){
   const target = e.target.closest('tr th');
+  if (!target) return;
   const table = e.target.closest('table');
   const tableBody = table.tBodies[0];
-if (!target) return;
+
 let sortIndex = target.cellIndex;
 let rows = [...table.tBodies[0].rows] ;
 let sortType = target.dataset.type;
